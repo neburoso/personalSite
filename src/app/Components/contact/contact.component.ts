498b56228f302396e4d6d47f6359a5041040a99e
Ruben Puerta Cordoba <>
2024-09-14 22:03:41 +0200

    update CV, update contact

--- src/app/Components/contact/contact.component.ts
+++ src/app/Components/contact/contact.component.ts
@@ -1,80 +1,11 @@
-import { Component, OnInit} from '@angular/core';
-// import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-// import { ContactService } from 'src/app/Services/contact.service';
-
-
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit{
+export class ContactComponent {
 
-    // contactMessage: contactDetails;
-
-  // name:FormControl;
-  // email:FormControl;
-  // message:FormControl;
-  // contactForm:FormGroup;
-  // isValidForm:boolean;
-
- 
-  constructor(
-  //  private formBuilder:FormBuilder,
-  //  private contactService: ContactService
-    
-  ) { 
-    // this.isValidForm=false;
-    // this.contactMessage={name:"", message:"", email:""}
-    // this.name=new FormControl(this.contactMessage.name)
-    // this.email= new FormControl(this.contactMessage.email)
-    // this.message= new FormControl(this.contactMessage.message)
-    // this.contactForm = this.formBuilder.group({
-    //   name:this.name,
-    //   email: this.email,
-    //   message:this.message
-    // })
-
-
-  }
-
-  ngOnInit(): void {
-
-    
-
-    // document.getElementById("sb-btn")?.addEventListener("click", function(submit){
-    //   let form= document.getElementsByTagName("form");
-    //   if(!form[0].checkValidity()){
-    //    submit.preventDefault()
-    //     form[0].reportValidity()
-    //   }
-    
-    //   })
-  }
-
-
-checkme(contact:any, e:Event){
-  e.preventDefault()
-
-  if(!contact.checkValidity()){
-    contact.reportValidity()
-  }else{
-    contact.action= "https://formsubmit.co/ruben@puertacordoba.com";
-    contact.submit()
-    contact.action= "#"
-  }
-
-  // let form= document.getElementsByTagName("form");
-  //   if(!form[0].checkValidity()){
-  //      form[0].reportValidity()
-  //      return;
-  //   }
-}
-  // sendMessage(){
-  // this.contactService.sendMessage(this.contactForm.value)
-  // console.log("sent")
-  // }
-
-
+  emailInfo= "ruben@puertacordoba.com"
 }
